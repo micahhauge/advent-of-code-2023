404d1271041496722df3efe5eac3551549c24e97
Micah <micahhauge@gmail.com>
2023-12-01 23:02:52 -0600

    wip

--- solutions/01/index.ts
+++ solutions/01/index.ts
@@ -4,9 +4,11 @@ import * as two from "./part-2";
 console.log(`Part 1: `, await one.solve());
 console.log(`Part 2: `, await two.solve());
 
-export async function parseInput(example = false) {
+export async function parseInput(example = true) {
   const file = `${import.meta.dir}/input${example ? "-example" : ""}.txt`;
-  const input = await Bun.file(file).text();
+  const input = (await Bun.file(file).text()) as string;
+
+  console.log(input);
 
   return input;
 }
--- solutions/01/part-1.ts
+++ solutions/01/part-1.ts
@@ -3,5 +3,7 @@ import { parseInput } from ".";
 export async function solve() {
   const input = await parseInput();
 
+  console.log(input);
+
   return "Not implemented.";
 }
