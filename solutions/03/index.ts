1cd339119a18b43d603c8d9ac43d72c4a97d3e01
Micah <micahhauge@gmail.com>
2023-12-03 15:05:06 -0600

    wip

--- solutions/03/index.ts
+++ solutions/03/index.ts
@@ -1,7 +1,7 @@
 import * as one from "./part-1";
 import * as two from "./part-2";
 
-console.log(`Part 1: `, await one.solve());
+// console.log(`Part 1: `, await one.solve());
 console.log(`Part 2: `, await two.solve());
 
 export async function parseInput(example = false) {
--- solutions/03/part-2.ts
+++ solutions/03/part-2.ts
@@ -1,38 +1,151 @@
-import { sum } from "lodash";
+import { sum, flatten } from "lodash";
 import { parseInput } from ".";
 
 export async function solve() {
   const input = await parseInput();
   console.log(input);
-  const parts: number[] = [];
-  let partsSum = 0;
+  let answer = 0;
 
   for (let y = 0; y < input.length; y++) {
     for (let x = 0; x < input[y].length; x++) {
-      // console.log(input[y][x]);
-      if (isDigit(input[y][x])) {
-        if (isDigit(input[y][x - 1])) {
-          continue;
-        }
+      if (isGear(input[y][x])) {
+        console.log("FOUND GEAR");
+        const adjacentNumbers = getNumbersAdjacentToGear(x, y, input);
 
-        const number = getEntireNumber(x, y, input);
-        const isAdjacentToSymbol = wholeNumberAdjacentToSymbol(
-          x,
-          y,
-          number,
-          input,
+        const parsedAdjacentNumbers = flatten(adjacentNumbers).map((n) =>
+          parseInt(n),
         );
-        console.log("found whole num", number, { isAdjacentToSymbol });
 
-        if (isAdjacentToSymbol) {
-          parts.push(parseInt(number));
-          partsSum += parseInt(number);
+        if (parsedAdjacentNumbers.length === 2) {
+          const gearRatio = parsedAdjacentNumbers[0] * parsedAdjacentNumbers[1];
+          console.log("RATIO", gearRatio);
+          answer += gearRatio;
         }
       }
     }
   }
 
-  return partsSum;
+  return answer;
+}
+
+function isGear(char: string) {
+  return char === "*";
+}
+
+function gearAdjacencyCount(x: number, y: number, input: string[]) {
+  const left = input[y]?.[x - 1];
+  const right = input[y]?.[x + 1];
+  const up = input[y - 1]?.[x];
+  const down = input[y + 1]?.[x];
+  const upRight = input[y - 1]?.[x + 1];
+  const upLeft = input[y - 1]?.[x - 1];
+  const downLeft = input[y + 1]?.[x - 1];
+  const downRight = input[y + 1]?.[x + 1];
+
+  const topCount = getFullNumberCountInThreesome(upLeft, up, upRight);
+  const leftCount = isDigit(left) ? 1 : 0;
+  const rightCount = isDigit(right) ? 1 : 0;
+  const bottomCount = getFullNumberCountInThreesome(downLeft, down, downRight);
+
+  return topCount + leftCount + rightCount + bottomCount;
+}
+
+function getNumbersAdjacentToGear(x: number, y: number, input: string[]) {
+  const left = input[y]?.[x - 1];
+  const right = input[y]?.[x + 1];
+  const up = input[y - 1]?.[x];
+  const down = input[y + 1]?.[x];
+  const upRight = input[y - 1]?.[x + 1];
+  const upLeft = input[y - 1]?.[x - 1];
+  const downLeft = input[y + 1]?.[x - 1];
+  const downRight = input[y + 1]?.[x + 1];
+
+  const topLeftOpt = isDigit(upLeft) ? "x" : "_";
+  const topMiddleOpt = isDigit(up) ? "x" : "_";
+  const topRightOpt = isDigit(upRight) ? "x" : "_";
+  const topOption: OptCombos = `${topLeftOpt}${topMiddleOpt}${topRightOpt}`;
+
+  const bottomLeftOpt = isDigit(downLeft) ? "x" : "_";
+  const bottomMiddleOpt = isDigit(down) ? "x" : "_";
+  const bottomRightOpt = isDigit(downRight) ? "x" : "_";
+  const bottomOption: OptCombos = `${bottomLeftOpt}${bottomMiddleOpt}${bottomRightOpt}`;
+
+  console.log({ topOption, bottomOption });
+
+  const topNumbers = getAdjacentNumbersForThreesome(
+    topOption,
+    x - 1,
+    y - 1,
+    input,
+  );
+  const leftNumbers = isDigit(left) ? [getEntireNumber(x - 1, y, input)] : [];
+  const rightNumbers = isDigit(right) ? [getEntireNumber(x + 1, y, input)] : [];
+  const bottomNumbers = getAdjacentNumbersForThreesome(
+    bottomOption,
+    x - 1,
+    y + 1,
+    input,
+  );
+
+  console.log({ topNumbers, leftNumbers, rightNumbers, bottomNumbers });
+
+  return [topNumbers, leftNumbers, rightNumbers, bottomNumbers];
+}
+
+type Opt = "x" | "_";
+type OptCombos = `${Opt}${Opt}${Opt}`;
+type OptComboToNumberCount = Record<OptCombos, number>;
+
+const optComboToNumberCount: OptComboToNumberCount = {
+  xxx: 1,
+  xx_: 1,
+  x_x: 2,
+  _xx: 1,
+  x__: 1,
+  _x_: 1,
+  __x: 1,
+  ___: 0,
+};
+
+function getAdjacentNumbersForThreesome(
+  opt: OptCombos,
+  x: number,
+  y: number,
+  input: string[],
+): string[] {
+  if (opt === "xxx" || opt === "xx_" || opt === "x__") {
+    return [getEntireNumber(x, y, input)];
+  }
+
+  if (opt === "x_x") {
+    return [getEntireNumber(x, y, input), getEntireNumber(x + 2, y, input)];
+  }
+
+  if (opt === "__x" || opt === "_xx") {
+    return [getEntireNumber(x + 2, y, input)];
+  }
+
+  if (opt === "_x_") {
+    return [getEntireNumber(x + 1, y, input)];
+  }
+
+  if (opt === "___") {
+    return [];
+  } else {
+    throw new Error(`Unknown opt: ${opt}`);
+  }
+}
+
+function getFullNumberCountInThreesome(
+  left: string,
+  middle: string,
+  right: string,
+) {
+  const leftOpt: Opt = isDigit(left) ? "x" : "_";
+  const middleOpt: Opt = isDigit(middle) ? "x" : "_";
+  const rightOpt: Opt = isDigit(right) ? "x" : "_";
+
+  return optComboToNumberCount[`${leftOpt}${middleOpt}${rightOpt}`];
 }
 
 function wholeNumberAdjacentToSymbol(
@@ -77,6 +190,14 @@ function digitAdjacentToSymbol(x: number, y: number, input: string[]) {
 function getEntireNumber(x: number, y: number, input: string[]) {
   let number = "";
   let i = x;
+
+  // move to left side of number
+  while (isDigit(input[y][i - 1])) {
+    i--;
+  }
+
+  console.log("entire", { x, y, char: input[y][i] });
+
   while (isDigit(input[y][i])) {
     number += input[y][i];
     i++;
