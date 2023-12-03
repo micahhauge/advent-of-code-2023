4e720f5c05a1a997421870ebdec9403d49a049ef
Micah <micahhauge@gmail.com>
2023-12-02 23:18:24 -0600

    wip

--- solutions/03/part-1.ts
+++ solutions/03/part-1.ts
@@ -4,5 +4,36 @@ export async function solve() {
   const input = await parseInput();
   console.log(input);
 
+  for (let y = 0; y < input.length; y++) {
+    for (let x = 0; x < input[y].length; x++) {
+      console.log(input[y][x]);
+      if (isDigit(input[y][x])) {
+        if (isDigit(input[y][x - 1])) {
+          continue;
+        }
+
+        const number = getEntireNumber(x, y, input);
+
+        console.log("found digit", input[x][y]);
+        console.log("found whole num", number);
+      }
+    }
+  }
+  console.log("here");
+
   return "Not implemented.";
+}
+
+function getEntireNumber(x: number, y: number, input: string[]) {
+  let number = "";
+  let i = x;
+  while (isDigit(input[y][i])) {
+    number += input[y][i];
+    i++;
+  }
+  return number;
+}
+
+function isDigit(char: string) {
+  return char >= "0" && char <= "9";
 }
