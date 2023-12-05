c151f0c562a58cdf71674fb1af1658cc72f8ef94
Micah <micahhauge@gmail.com>
2023-12-05 06:30:20 -0600

    brute force

--- solutions/05/index.ts
+++ solutions/05/index.ts
@@ -4,7 +4,7 @@ import * as two from "./part-2";
 console.log(`Part 1: `, await one.solve());
 // console.log(`Part 2: `, await two.solve());
 
-export async function parseInput(example = true) {
+export async function parseInput(example = false) {
   const file = `${import.meta.dir}/input${example ? "-example" : ""}.txt`;
   const input = await Bun.file(file).text();
 
@@ -20,21 +20,34 @@ export async function parseInput(example = true) {
 
     const mapValues = rawMapValues.map((rawMapValueRow) => {
       const [destStart, sourceStart, rangeLength] = rawMapValueRow.split(" ");
-      return { destStart, sourceStart, rangeLength };
+      return {
+        destStart: Number(destStart),
+        sourceStart: Number(sourceStart),
+        rangeLength: Number(rangeLength),
+      };
     });
 
-    return { mapName, sourceItemName, destItemName, mapValues };
+    const map: Record<string, string> = {};
+    mapValues.forEach(({ destStart, sourceStart, rangeLength }) => {
+      for (let i = 0; i < rangeLength; i++) {
+        const dest = destStart + i;
+        const source = sourceStart + i;
+        map[source] = `${dest}`;
+      }
+    });
+
+    return { mapName, sourceItemName, destItemName, mapValues, map };
   });
 
-  type MapItem = (typeof maps)[0];
-
-  const mapOfMaps = maps.reduce<Record<string, MapItem>>(
-    (acc, map) => ({ ...acc, [map.mapName]: map }),
+  const mapOfMaps = maps.reduce<Record<string, Record<string, string>>>(
+    (acc, map) => ({ ...acc, [map.mapName]: map.map }),
     {},
   );
 
-  return [inputSeeds, mapOfMaps] as [typeof inputSeeds, typeof mapOfMaps];
+  return { inputSeeds, mapOfMaps };
 }
+
+export type MapName = `${ItemName}-to-${ItemName}`;
 
 export type ItemName =
   | "seed"
--- solutions/05/part-1.ts
+++ solutions/05/part-1.ts
@@ -1,8 +1,46 @@
 import { parseInput } from ".";
+import { min } from "lodash";
 
 export async function solve() {
-  const [inputSeeds, mapOfMaps] = await parseInput();
-  console.log(inputSeeds, mapOfMaps);
+  const { inputSeeds, mapOfMaps } = await parseInput();
 
-  return "Not implemented.";
+  function seedValueToLocation(seedValue: string) {
+    const soil = mapOfMaps["seed-to-soil"][seedValue] || seedValue;
+    const fertilizer = mapOfMaps["soil-to-fertilizer"][soil] || soil;
+    const water = mapOfMaps["fertilizer-to-water"][fertilizer] || fertilizer;
+    const light = mapOfMaps["water-to-light"][water] || water;
+    const temperature = mapOfMaps["light-to-temperature"][light] || light;
+    const humidity =
+      mapOfMaps["temperature-to-humidity"][temperature] || temperature;
+    const location = mapOfMaps["humidity-to-location"][humidity] || humidity;
+
+    // console.log({
+    //   seedValue,
+    //   soil,
+    //   fertilizer,
+    //   water,
+    //   light,
+    //   temperature,
+    //   humidity,
+    //   location,
+    // });
+
+    return location;
+  }
+
+  const allLocations = inputSeeds.map(seedValueToLocation);
+
+  console.log("seed 79", seedValueToLocation("79"));
+
+  return min(allLocations);
 }
+
+export type ItemName =
+  | "seed"
+  | "soil"
+  | "fertilizer"
+  | "water"
+  | "light"
+  | "temperature"
+  | "humidity"
+  | "locations";
