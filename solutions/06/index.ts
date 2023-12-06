e33f4e5664014319877e124d1f74b79e136fefb0
Micah <micahhauge@gmail.com>
2023-12-05 23:09:39 -0600

    parse

--- solutions/06/index.ts
+++ solutions/06/index.ts
@@ -2,11 +2,24 @@ import * as one from "./part-1";
 import * as two from "./part-2";
 
 console.log(`Part 1: `, await one.solve());
-console.log(`Part 2: `, await two.solve());
+// console.log(`Part 2: `, await two.solve());
 
 export async function parseInput(example = true) {
   const file = `${import.meta.dir}/input${example ? "-example" : ""}.txt`;
-  const input = await Bun.file(file).text();
+  const input = (await Bun.file(file).text()).split("\n");
 
-  return input;
+  const times = input[0]
+    .trim()
+    .split(": ")[1]
+    .trim()
+    .split(/\s+/)
+    .map((t) => Number(t));
+  const distanceRecords = input[1]
+    .trim()
+    .split(":")[1]
+    .trim()
+    .split(/\s+/)
+    .map((t) => Number(t));
+
+  return { times, distanceRecords };
 }
