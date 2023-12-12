71f33c180f283c1ea39c4233c7a386031d6bb4bc
Micah <micahhauge@gmail.com>
2023-12-12 16:55:18 -0600

    wip

--- solutions/06/part-2.ts
+++ solutions/06/part-2.ts
@@ -5,52 +5,131 @@ export async function solve() {
   const { races } = await parseInput();
   const race = races[0];
 
-  const times = range(0, race.time + 1);
-  const fastestTime = fastestTimeBinarySearch(times);
-
-  const rightTime = getRightTime(fastestTime);
-  const leftTime = getLeftTime(fastestTime);
-
-  const solutionCount =
-    getRightTime(fastestTime) - getLeftTime(fastestTime) + 1;
-
-  console.table({ leftTime, rightTime, solutionCount });
-
-  function getLeftTime(fastestTime: number) {
-    let time = fastestTime;
-    while (distance(time) > race.distanceRecord) {
-      time--;
+  function getWorstWinningTime() {
+    for (let i = 0; i < race.time; i++) {
+      if (distance(i) > race.distanceRecord) {
+        return i;
+      }
     }
-    return time + 1;
+    return -1;
   }
 
-  function getRightTime(fastestTime: number) {
-    let time = fastestTime;
-    while (distance(time) > race.distanceRecord) {
-      time++;
+  function getBestWinningTime() {
+    for (let i = race.time; i > 0; i--) {
+      if (distance(i) > race.distanceRecord) {
+        return i;
+      }
     }
-    return time - 1;
+    return -1;
+  }
+
+  const worstWinningTime = getWorstWinningTime();
+  const bestWinningTime = getBestWinningTime();
+
+  console.table({ worstWinningTime, bestWinningTime });
+
+  const answer = bestWinningTime - worstWinningTime;
+
+  function distance(waitTime: number) {
+    return performantDistanceForTime(waitTime, race.time);
+  }
+  return answer;
+}
+
+export async function solveOld() {
+  const { races } = await parseInput();
+  const race = races[0];
+
+  // const times = range(0, race.time + 1);
+  // const fastestTime = fastestTimeBinarySearch(times);
+
+  // console.log({ fastestTime });
+
+  // const rightTime = rightTimeBinarySearch(times, fastestTime);
+  // const leftTime = leftTimeBinarySearch(times, fastestTime);
+
+  // const solutionCount = rightTime - leftTime;
+  // console.log({ fastestTime, leftTime, rightTime, solutionCount });
+
+  // const f = distance(35765);
+  // const fMin1 = distance(35764);
+  // const fPlus1 = distance(35764);
+
+  // console.log({ f, fMin1, fPlus1 });
+
+  function leftTimeBinarySearch(fastestTime: number) {
+    let left: number = 0;
+    let right: number = fastestTime - 1;
+
+    while (left <= right) {
+      const mid: number = Math.floor((left + right) / 2);
+
+      // if the middle is the point, return it
+      if (
+        distance(mid) <= distance(fastestTime) &&
+        distance(mid + 1) > distance(fastestTime)
+      ) {
+        return mid;
+      }
+
+      if (distance(mid) <= race.distanceRecord) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+    return -1;
+  }
+
+  function rightTimeBinarySearch(times: number[], fastestTime: number) {
+    let left: number = fastestTime + 1;
+    let right: number = times.length - 1;
+
+    while (left <= right) {
+      const mid: number = Math.floor((left + right) / 2);
+
+      // if the middle is the max, return it
+      if (distance(mid) === race.distanceRecord) {
+        return mid;
+      }
+
+      if (distance(mid) > race.distanceRecord) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+
+    return -1;
   }
 
   function fastestTimeBinarySearch(times: number[]) {
     let left: number = 0;
     let right: number = times.length - 1;
 
-    while (left !== right) {
+    while (left <= right) {
       const mid: number = Math.floor((left + right) / 2);
 
-      if (distance(times[mid]) < distance(times[mid + 1])) {
+      // if the middle is the max, return it
+      if (
+        mid === 0 ||
+        (distance(mid) > distance(mid + 1) && distance(mid) > distance(mid - 1))
+      ) {
+        return mid;
+      }
+
+      if (mid > 0 && distance(mid) < distance(mid + 1)) {
         left = mid + 1;
       } else {
-        right = mid;
+        right = mid - 1;
       }
     }
 
-    return left;
+    return -1;
   }
 
   function distance(waitTime: number) {
-    return getDistanceForTime(waitTime, race.time);
+    return performantDistanceForTime(waitTime, race.time);
   }
 
   return solutionCount;
@@ -102,6 +181,12 @@ function waysToWinRace(race: Race) {
     }
   }
   return ways;
+}
+
+function performantDistanceForTime(timeHeld: number, totalTime: number) {
+  const speed = timeHeld;
+  const timeLeft = totalTime - timeHeld;
+  return speed * timeLeft;
 }
 
 function getDistanceForTime(timeHeld: number, totalTime: number) {
