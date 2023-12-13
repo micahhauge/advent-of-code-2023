1c74bf35f1aaa8c3a6d65589aebdf6e76720492a
Micah <micahhauge@gmail.com>
2023-12-12 19:35:47 -0600

    start 12

--- solutions/12/index.ts
+++ solutions/12/index.ts
@@ -4,9 +4,22 @@ import * as two from "./part-2";
 console.log(`Part 1: `, await one.solve());
 console.log(`Part 2: `, await two.solve());
 
-export async function parseInput(example = false) {
+export type SpringData = {
+  springs: string;
+  damagedGroups: number[];
+};
+
+export async function parseInput(example = true) {
   const file = `${import.meta.dir}/input${example ? "-example" : ""}.txt`;
   const input = await Bun.file(file).text();
+  const lines = input.split("\n").filter(Boolean);
 
-  return input;
+  const parsedLines = lines.map<SpringData>((line) => {
+    const [springs, rawDamagedGroups] = line.split(" ");
+    const damagedGroups = rawDamagedGroups.split(",").map(Number);
+
+    return { springs, damagedGroups };
+  });
+
+  return parsedLines;
 }
--- solutions/12/part-1.ts
+++ solutions/12/part-1.ts
@@ -1,7 +1,19 @@
 import { parseInput } from ".";
+import { sum } from "lodash";
 
 export async function solve() {
   const input = await parseInput();
 
-  return "Not implemented.";
+  const arrs = input.map((i) =>
+    getPossibleSpringArrangements(i.springs, i.damagedGroups),
+  );
+
+  return sum(arrs);
+}
+
+function getPossibleSpringArrangements(
+  springs: string,
+  damagedGroups: number[],
+) {
+  return 0;
 }
