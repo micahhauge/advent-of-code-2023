c1b5a9ade53d82b8ae68397b7a8f6ad61052d5bd
Micah <micahhauge@gmail.com>
2024-12-05 12:25:27 -0600

    wip

--- 2024/solutions/05/index.ts
+++ 2024/solutions/05/index.ts
@@ -4,9 +4,22 @@ import * as two from "./part-2";
 console.log(`Part 1: `, await one.solve());
 console.log(`Part 2: `, await two.solve());
 
+type RuleTuple = [number, number];
+
 export async function parseInput(example = false) {
   const file = `${import.meta.dir}/input${example ? "-example" : ""}.txt`;
   const input = await Bun.file(file).text();
 
-  return input;
+  const [rulesString, updatesString] = input.split("\n\n");
+
+  const rules = rulesString.split("\n").map<RuleTuple>((rule) => {
+    const [left, right] = rule.split("|");
+    return [Number(left), Number(right)];
+  });
+
+  const updates = updatesString
+    .split("\n")
+    .map((updateString) => updateString.split(",").map(Number));
+
+  return { rules, updates };
 }
--- 2024/solutions/05/part-1.ts
+++ 2024/solutions/05/part-1.ts
@@ -1,7 +1,9 @@
 import { parseInput } from ".";
 
 export async function solve() {
-  const input = await parseInput();
+  const { rules, updates } = await parseInput(true);
+
+  console.log({ rules, updates });
 
   return "Not implemented.";
 }
