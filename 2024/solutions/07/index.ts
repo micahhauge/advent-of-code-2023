f55edacff5947c1e5c6f7db7de7b2d1bcc6e86e4
Micah <micahhauge@gmail.com>
2024-12-07 00:10:31 -0600

    wip

--- 2024/solutions/07/index.ts
+++ 2024/solutions/07/index.ts
@@ -4,9 +4,28 @@ import * as two from "./part-2";
 console.log(`Part 1: `, await one.solve());
 console.log(`Part 2: `, await two.solve());
 
+export type Equation = {
+  testValue: number;
+  numbers: number[];
+};
+
 export async function parseInput(example = false) {
   const file = `${import.meta.dir}/input${example ? "-example" : ""}.txt`;
   const input = await Bun.file(file).text();
 
-  return input;
+  const lines = input.split("\n");
+
+  const equations = lines.map((line) => {
+    const [testValueString, numbersString] = line.split(": ");
+
+    const testValue = Number(testValueString);
+    const numbers = numbersString.split(" ").map(Number);
+
+    return {
+      testValue,
+      numbers,
+    };
+  });
+
+  return equations;
 }
--- 2024/solutions/07/part-1.ts
+++ 2024/solutions/07/part-1.ts
@@ -1,7 +1,117 @@
-import { parseInput } from ".";
+import { sum } from "lodash";
+import { Equation, parseInput } from ".";
+
+type Operator = "+" | "*";
+type EquationPart = {
+  number: number;
+  operator: Operator;
+};
 
 export async function solve() {
   const input = await parseInput();
 
-  return "Not implemented.";
+  const possibleEquations = input.filter(equationIsPossible);
+
+  const answer = sum(possibleEquations.map((equation) => equation.testValue));
+
+  return answer;
 }
+
+function equationIsPossible(equation: Equation) {
+  const permutations = getPermutations(equation.numbers);
+
+  for (let i = 0; i < permutations.length; i++) {
+    const permutation = permutations[i];
+    const answer = calculateEquationAnswer(permutation);
+
+    if (answer === equation.testValue) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+function calculateEquationAnswer(parts: EquationPart[]): number {
+  let answer = 0;
+
+  parts.forEach((part) => {
+    if (part.operator === "+") {
+      answer += part.number;
+    } else if (part.operator === "*") {
+      answer *= part.number;
+    } else {
+      throw new Error(`Invalid operator: ${part.operator}`);
+    }
+  });
+
+  return answer;
+}
+
+function getPermutations(numbers: number[]): EquationPart[][] {
+  let permutations: EquationPart[][] = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    const additionsForThisPart: EquationPart[] = [
+      { number: numbers[i], operator: "+" },
+      { number: numbers[i], operator: "*" },
+    ];
+
+    if (i === 0) {
+      permutations.push([{ number: numbers[i], operator: "+" }]);
+      continue;
+    }
+
+    const newPermutations: EquationPart[][] = [];
+
+    permutations.forEach((permutation) => {
+      additionsForThisPart.forEach((addition) => {
+        newPermutations.push([...permutation, addition]);
+      });
+    });
+
+    permutations = newPermutations;
+  }
+
+  return permutations;
+}
+
+/**
+ 
+
+10 19
+
+10 + 19
+10 - 19
+
+0:
+81 40 27
+
+1:
+121
+41
+
+2:
+148
+94
+68
+67
+
+0:
+81 40 27
+
+1:
+81 + 40
+81 - 40
+
+2:
+
+81 + 40 + 27
+81 + 40 - 27
+81 - 40 + 27
+81 - 40 - 27
+
+
+
+
+ */
